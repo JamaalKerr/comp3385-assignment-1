317a6ed183d0448bce8d6ad2d53b72b6e104ca25
Jamaal Kerr <kerrjamaal994@gmail.com>
2025-02-10 18:53:05 -0400

    Add CSS styles and image to About page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Jamaal Kerr</h2>
+    <p>My name is Jamaal Kerr, I’m 22, and I love the gym, food, and staying active. I’m currently studying Computer Science at the University of the West Indies, where I’m working hard to build a future in tech. Balancing fitness, food, and academics keeps me motivated and focused on my goals.</p>
+    <img src="{{ url('/images/spaceship-image.jpg') }}" alt="flying spaceship" class="responsive-image">
+@endsection
